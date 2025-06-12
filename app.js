95fcd36d36972efc1c9c0b69be24268191cba2aa
inayahayudeswita <nayahdeswita@gmail.com>
2025-06-12 14:57:00 +0700

    Add AboutUs route to app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const transactionController = require("./controllers/contentController/transactionController");
 const ourPartnerController = require("./controllers/contentController/ourPartnerController");
-const aboutUsRoutes = require("./routes/aboutUsRoutes");
+const aboutUsRoutes = require("./routes/aboutusRoutes");
 const midtransPolling = require("./midtransPolling");
 const { connectDB } = require("./config/db");
 
@@ -26,7 +26,7 @@ app.use(cors({
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/v1/content/aboutus", aboutUsRoutes);
+app.use("/v1/content/aboutus", aboutusRoutes);
 
 
 // Routes
