7db799f284ba2a65848986390ab172bc5617e36c
inayahayudeswita <nayahdeswita@gmail.com>
2025-09-06 19:08:11 +0700

    new

--- generateWebhookPayload.js
+++ generateWebhookPayload.js
@@ -29,7 +29,7 @@ payload.signature_key = signature_key;
 console.log("🚀 Payload siap dikirim:", payload);
 
 // 🌐 Ganti URL ini dengan alamat ngrok aktif kamu
-const webhookUrl = "https://1234-abcd.ngrok-free.app/v1/content/transaction/notification";
+const webhookUrl = "https://backendd-fundunity.onrender.com/api/midtrans/notification";
 
 // Kirim request ke webhook endpoint (POST)
 axios
